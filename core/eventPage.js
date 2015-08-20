e103adaf7405debd829d6b6e81019c1a4a96600b
mleboeuf <ml@maximeleboeuf.com>
2015-08-19 23:07:58 -0400

    Experimentation with eventPage

--- core/eventPage.js
+++ core/eventPage.js
@@ -1,6 +1,12 @@
+// This is triggered when a document has finished loading
+// see http://stackoverflow.com/questions/27239280 for pushstate (e.g. youtube pages that load via ajax)
 chrome.webNavigation.onCompleted.addListener(function(o) {
+
+	// Only act on the main page
+	if (o.frameId > 0) return;
+
 	chrome.tabs.executeScript(o.tabId, {
-		code: "alert('ok');"
+		code: 'document.body.style.backgroundColor="red"'
 	});
 }, {
 	url: []
