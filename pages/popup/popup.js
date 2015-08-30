b8f87c1a83090407313b0b44113f0292298d2045
mleboeuf <ml@maximeleboeuf.com>
2015-08-30 11:24:16 -0400

    Popup script

--- pages/popup/popup.js
+++ pages/popup/popup.js
@@ -17,11 +17,26 @@ function toggleEnabledOnThisSite(isEnabled) {
 }
 
 function editCustomCss() {
-
+	console.log(getCurrentTabUrl());
 }
 
 function editCustomJs() {
 	
 }
+
+function getCurrentTabUrl() {
+	var url;
+	chrome.tabs.query({active: true, currentWindow: true}, function(arrayOfTabs) {
+		// http://stackoverflow.com/questions/6132018
+		// Since only one tab should be active and in the current window at once
+		// the return variable should only have one entry
+		var activeTab = arrayOfTabs[0];
+		url = activeTab.url;
+	});
+	return url;
+	// see: http://stackoverflow.com/questions/18592705 for async
+}
+
+
 
 //http://stackoverflow.com/questions/9915311/chrome-extension-code-vs-content-scripts-vs-injected-scripts
